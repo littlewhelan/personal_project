5b43fc140303b039b63a0e755186a98e7cc194f9
littlewhelan <klwhelan@gmail.com>
2015-09-24 23:50:46 -0500

    score updates after split and can play out original hand

--- models/split.js
+++ models/split.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var score = require('../logic/scoreVars');
 var track = require ('../logic/trackingDeck');
+var calc = require('../logic/calcScores');
 
 var splitOriginalHand = function() {
 
@@ -9,6 +10,7 @@ var splitOriginalHand = function() {
         //need to move one card to split array
         var split1 = track.playerArray.shift();
        track.split1Array.push(split1);
+        calc.playerScoreF();
     }
 };
 
